308bb1f3fe65cd79953735b92c4721f1609924d1
Arthur Flôr <arthurflor23@gmail.com>
2020-11-18 20:42:31 -0300

    Hello world extension refactory

--- qlik/hello-world/hello-world.js
+++ qlik/hello-world/hello-world.js
@@ -1,131 +1,69 @@
 define([
+    'qlik',
     './properties',
     'text!./hello-world.ng.html',
     'css!./hello-world.ng.css'
   ],
-  function (props, template, style) {
+  function (qlik, props, template, style) {
     'use strict';
-
-    // function scape(value, tid) {
-    //   var card = angular.element.find(`[tid='${tid}']`)[0];
-
-    //   var button = `
-    //   <div class="qs-popover-container qs-toolbar__element extra-button">
-    //     <button type="button" class="lui-button" tid="_007" qva-outside-ignore-for="nav-bookmark" title="">
-    //       <span class="lui-icon lui-icon--drop qs-no-margin" aria-hidden="true"></span>
-    //     </button>
-    //   </div>`;
-
-    //   // var createButton = false ? angular.element.find(`[tid='_007']`) : true;
-
-    //   // console.log(createButton);
-
-    //   // if (createButton) {
-    //     var group = angular.element.find(`[class='qs-toolbar__right']`)[0];
-    //     angular.element(group).prepend(angular.element(button));
-    //   // }
-
-    //   if (value) {
-    //     angular.element(card).addClass("scape");
-    //   } else {
-    //     angular.element(card).removeClass("scape");
-    //   }
-    // }
 
     return {
       definition: props,
       template: template,
       style: style,
       controller: ['$scope', '$compile', function ($scope, $compile) {
-
-        angular.element(`#qs-toolbar-container`).addClass('cubricks-navbar');
-        angular.element(`#qs-toolbar-container`).addClass('navbar-cubricks');
-
-        // scape($scope.layout.props.scape, $scope.layout.qInfo.qId);
-        // console.log($scope.layout);
-
-        var navbar = angular.element(`<div id="cubricks-navbar" class="navbar"></div>`);
-
-        var btn = angular.copy(angular.element(`[tid='2f7a7e']`));
-        // var btn = angular.element(`[tid='2f7a7e']`);
-
-        navbar.append($compile(btn)($scope));
-
-        // var myClone = element.clone();
-        // myClone[0].innerHTML = element[0].srcHTML;
-        // // $compile(myClone)($scope);
-
-        // navbar.append( $compile(myClone)($scope));
-
-        //  var pElement = angular.element(`[tid='2f7a7e']`);
-        //  var pElementCopy = pElement.clone();
-        //  pElementCopy.html("Foo");
-
-        //  $compile(pElementCopy)($scope);
-        //  navbar.append(pElementCopy);
-
-
-        // navbar.append(angular.element(`[tid='2f7a7e']`));
-        // navbar.append(angular.element(`[tid='4dd782']`));
-
-
-        navbar.append(angular.element(`
-        <div id="cubricks-grroup-btn" class="lui-buttongroup">
-          <lui-button ng-click="qlik.navigation.nextSheet()">Next sheet</lui-button>
-          <lui-button ng-click="qlik.navigation.prevSheet()">Previous sheet</lui-button>
-          <lui-button tid='4dd782'>Edit mode</lui-button>
-          <lui-button ng-click="scape()">Edit mode</lui-button>
-          <button type="button" class="lui-button lui-buttongroup__button" tid="4dd782" title="Edit sheet">
-            <span class="lui-icon lui-icon--edit lui-button__icon" aria-hidden="true"></span>
-          </button>
-        </div>`));
-
-
-        var container = angular.element('#qs-toolbar-container');
-        container.append($compile(navbar)($scope));
-
-
-        $scope.scape = function () {
-          console.log(' TEMP ');
+        $scope.initElements = async function (key) {
+          await (async (k) => $scope.key = k)(key);
+          $('#qs-toolbar-container').append($(`#${key}-navbar`));
         }
 
-
-        $scope.component.model.Validated.bind(function () {
-          // scape($scope.layout.props.scape, $scope.layout.qInfo.qId);
-          console.log($scope.layout.props);
-
-
-          // var style = document.createElement('style');
-          // style.setAttribute("id", "cubricks");
-
-          // style.innerHTML = `
-          // .cubricks-temp {
-          //   color: ${$scope.layout.props.section1?.item2 || 'red'} !important;
-          // }`;
-          
-          // angular.element('#cubricks').remove();
-          // angular.element('head')[0].appendChild(style);
-
-
-          // var element = angular.element(".lui-icon.lui-icon--more-rounded.lui-fade-button__icon");
-          // element.addClass('cubricks-temp');
-
-        });
-
-        $scope.$on('$destroy', function () {
+        $scope.destroyElements = function () {
           $scope.component.model.Validated.unbind();
-
-          angular.forEach($("[class*='cubricks']"), (element) => {
-            angular.element(element).removeClass((_, classes) => {
-              return classes.split(' ').filter((w) => w.includes('cubricks')).join(' ');
+          angular.forEach($(`[id*='${$scope.key}']`), (elem) => $(elem).remove());
+          angular.forEach($(`[class*='${$scope.key}']`), (elem) => {
+            $(elem).removeClass((_, style) => {
+              return style.split(' ').filter((w) => w.includes($scope.key)).join(' ');
             });
           });
+        }
 
-          angular.forEach($("[id*='cubricks']"), (element) => {
-            angular.element(element).remove();
-          });
-        });
+        $scope.changePath = function (path, option) {
+          var dividor = '/state/';
+          var index = window.location.pathname.indexOf(dividor);
+          var pathname = window.location.pathname.substring(0, index + dividor.length);
+          window.open(pathname + path, option);
+        }
 
+        // $scope.component.model.Validated.bind(() => $scope.initElements());
+        $scope.$on('$destroy', () => $scope.destroyElements());
+        $scope.initElements(props.key);
+
+
+        // qlik.navigation.prevSheet();
+        // qlik.navigation.nextSheet();
+        // qlik.navigation.setMode('edit');
+        // qlik.navigation.setMode('analysis');
+
+
+
+        // var element = '<div><div role="dialog" class="lui-popover" id="rlui-popover-1" style="visibility: visible; position: absolute; max-width: 500px; top: 44.5px; left: 10px;"><div><ul class="lui-list qs-toolbar__menu" data-tid="nav-menu" role="menu"><div><li class="lui-list__item qs-toolbar__menu-item" role="menuitem" tid="4dfdd3" title="App overview" tabindex="0"><div class="lui-list__aside  qs-toolbar__menu-item-icon"><span class="lui-icon lui-icon--home" aria-hidden="true"></span></div><div class="lui-list__text">App overview</div><div class="list-divider"></div><div class="lui-list__aside  lui-nopad"><button type="button" class="lui-fade-button" title="Open in new tab" tabindex="-1"><span class="lui-icon lui-icon--new-tab lui-fade-button__icon" aria-hidden="true" style="text-align: center; margin: auto;"></span></button></div></li><li class="lui-list__item qs-toolbar__menu-item" role="menuitem" tid="4dfdd2" title="Open hub in a new tab" tabindex="0"><div class="lui-list__aside  qs-toolbar__menu-item-icon"><span class="lui-icon lui-icon--hub-logo" aria-hidden="true"></span></div><div class="lui-list__text">Open hub</div><div class="lui-list__aside  lui-nopad"><button type="button" class="lui-fade-button" title="Open in new tab" tabindex="-1"><span class="lui-icon lui-icon--new-tab lui-fade-button__icon" aria-hidden="true" style="text-align: center; margin: auto;"></span></button></div></li></div><div class="qs-toolbar__divider"></div><div><li class="lui-list__item qs-toolbar__menu-item" role="menuitem" tid="GlobalMenu.ExportSheetToPdf" title="Download sheet as PDF" tabindex="0"><div class="lui-list__aside  qs-toolbar__menu-item-icon"><span class="lui-icon lui-icon--download" aria-hidden="true"></span></div><div class="lui-list__text">Download sheet as PDF</div></li><li class="lui-list__item qs-toolbar__menu-item" role="menuitem" tid="GlobalMenu.DuplicateSheet" title="Duplicate sheet" tabindex="0"><div class="lui-list__aside  qs-toolbar__menu-item-icon"><span class="lui-icon lui-icon--copy" aria-hidden="true"></span></div><div class="lui-list__text">Duplicate sheet</div></li><li class="lui-list__item qs-toolbar__menu-item" role="menuitem" tid="GlobalMenu.DeleteSheet" title="Delete sheet" tabindex="0"><div class="lui-list__aside  qs-toolbar__menu-item-icon"><span class="lui-icon lui-icon--bin" aria-hidden="true"></span></div><div class="lui-list__text">Delete sheet</div></li><li class="lui-list__item qs-toolbar__menu-item" role="menuitem" tid="i9fd7" title="Embed sheet" tabindex="0"><div class="lui-list__aside  qs-toolbar__menu-item-icon"><span class="lui-icon lui-icon--code" aria-hidden="true"></span></div><div class="lui-list__text">Embed sheet</div></li><li class="lui-list__item qs-toolbar__menu-item no-hover" role="menuitem" tid="4dfe29" title="Touch screen mode" tabindex="-1"><div class="lui-list__aside  qs-toolbar__menu-item-icon"><span class="lui-icon lui-icon--touch" aria-hidden="true"></span></div><div class="lui-list__text"><div class="qs-toolbar__toggle-touch"><div class="qs-toolbar__toggle-touch-label">Touch screen mode<strong>Off</strong></div><div class="qs-toolbar__toggle-touch-switch"><div class="lui-switch"><label class="lui-switch__label"><input class="lui-switch__checkbox" type="checkbox"><span class="lui-switch__wrap"><div class="lui-switch__inner"></div><div class="lui-switch__switch"></div></span></label></div></div></div></div></li><li class="lui-list__item qs-toolbar__menu-item" role="menuitem" tid="4dfe26" title="Open the help site in new tab" tabindex="0"><div class="lui-list__aside  qs-toolbar__menu-item-icon"><span class="lui-icon lui-icon--help" aria-hidden="true"></span></div><div class="lui-list__text">Help</div></li><li class="lui-list__item qs-toolbar__menu-item" role="menuitem" tid="4dfe27" title="About" tabindex="0"><div class="lui-list__aside  qs-toolbar__menu-item-icon"><span class="lui-icon lui-icon--info" aria-hidden="true"></span></div><div class="lui-list__text">About</div></li></div></ul></div><div class="lui-popover__arrow  lui-popover__arrow--top" style="left: 30px;"></div></div></div>'
+        // $compile(element)($scope);
+        // angular.element('.qs-popover-container.qs-toolbar__element').append(element);
+
+        // var pElement = angular.element('#qs-toolbar-container');
+        // var pElementCopy = pElement.clone();
+        // $compile(pElementCopy)($scope);
+        // angular.element('.qs-header').append(pElementCopy);
+
+
+        // $('.qs-header').append($compile($(`#qs-toolbar-container`).clone())($scope));
+        // $('#qs-toolbar-container').append($compile($(`[tid='2f7a7e']`).clone())($scope));
+
+
+        // console.log(qlik.navigation);
+        // console.log(qlik.navigation.getCurrentSheetId());
+        console.log($scope.layout);
+        // console.log(qlik);
       }]
     };
   });
--- qlik/hello-world/properties.js
+++ qlik/hello-world/properties.js
@@ -1,116 +1,26 @@
 define([], function () {
-    'use strict';
-    // *****************************************************************************
-    // Dimensions, Measures, Sorting
-    // *****************************************************************************
-    var dimensions = {
-        uses: 'dimensions',
-        min: 0,
-        max: 1
-    };
-    var measures = {
-        uses: 'measures',
-        min: 0,
-        max: 1
-    };
-    var sorting = {
-        uses: 'sorting'
-    };
-    // *****************************************************************************
-    // Custom section
-    // *****************************************************************************
-    var header1_item1 = {
-        ref: "props.section1.item1",
-        label: "Section 1 / Item 1",
-        type: "string",
-        expression: "optional"
-    };
-    var header1_item2 = {
-        ref: "props.section1.item2",
-        label: "Section 1 / Item 2",
-        type: "string",
-        expression: "optional"
-    };
-    var header2_item1 = {
-        ref: "props.section2.item1",
-        label: "Section 2 / Item 1",
-        type: "string",
-        expression: "optional"
-    };
-    var header2_item2 = {
-        ref: "props.section2.item2",
-        label: "Section 2 / Item 2",
-        type: "number",
-        expression: "optional"
-    };
+  'use strict';
 
-    var myCustomSection = {
-        type: "items",
-        component: "expandable-items",
-        label: "My Accordion Section",
-        items: {
-            header1: {
-                type: "items",
-                label: "Header 1",
-                items: {
-                    header1_item1: header1_item1,
-                    header1_item2: header1_item2
-                }
-            },
-            header2: {
-                type: "items",
-                label: "Header 2",
-                items: {
-                    header2_item1: header2_item1,
-                    header2_item2: header2_item2
-                }
-            }
+  var custom = {
+    label: 'Custom',
+    ref: 'props.custom',
+    layoutRef: 'custom',
+    type: 'string',
+    component: 'string',
+    defaultValue: 'TEMP VALUE',
+  }
 
-        }
+  var appearance = {
+    uses: 'settings',
+  };
+
+  return {
+    key: 'cbk',
+    type: 'items',
+    component: 'accordion',
+    items: {
+      appearance: appearance,
+      custom: custom,
     }
-
-    var myMedia = {
-        label: "My media",
-        component: "media",
-        ref: "props.myMedia",
-        layoutRef: "myMedia",
-        type: "string"
-    }
-
-    // *****************************************************************************
-    // Appearance section
-    // *****************************************************************************
-    var appearance = {
-        uses: 'settings',
-    };
-    // *****************************************************************************
-    // Main properties panel definition
-    // Only what is defined here is returned from properties.js
-    // *****************************************************************************
-    return {
-        type: 'items',
-        component: 'accordion',
-        items: {
-            dimensions: dimensions,
-            measures: measures,
-            sorting: sorting,
-            appearance: appearance,
-            myCustomSection: myCustomSection,
-            myMedia: myMedia,
-            MySwitchProp: {
-                type: "boolean",
-                component: "switch",
-                label: "Switch me On",
-                ref: "props.scape",
-                options: [{
-                    value: true,
-                    label: "On"
-                }, {
-                    value: false,
-                    label: "Not On"
-                }],
-                defaultValue: true
-            }
-        }
-    };
+  };
 });
