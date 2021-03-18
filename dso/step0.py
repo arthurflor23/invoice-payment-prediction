a965c15103653b582009e1cca42e3927a8050ea6
Arthur Flôr <arthurflor23@gmail.com>
2021-03-18 18:36:21 -0300

    Update step0 script

--- dso/step0.py
+++ dso/step0.py
@@ -1,46 +1,34 @@
-import numpy as np
 import pandas as pd
 
 
 def load_dataset(filename):
     df = pd.read_csv(filename, low_memory=False)
+    print('Total size:\t\t\t\t', len(df))
 
-    print("Total size:")
-    print(df.info(), '\n\n')
-
-    # Filter transaction type equal to invoiced
     df = df[df['TransactionType'] == 'FI-InvoicedDocument']
+    print('Total size (transaction type filter):\t', len(df))
 
-    print("Total size (transaction type filter):")
-    print(df.info(), '\n\n')
-
-    # Select columns of interest
     df = df[[
         'DocumentKey',
         'CustomerKey',
         'DocumentDate',
         'DueDate',
         'ClearingDate',
-        'DocumentType',
-        'PaymentTerms',
         'InvoicedAmount',
     ]].dropna()
 
-    print("Total size (dropna filter):")
-    print(df.info(), '\n\n')
+    print('Total size (dropna filter):\t\t', len(df), '\n')
 
-    # Cast dTypes of the columns
     for col in ['DocumentKey']:
-        df[col] = df[col].apply(lambda x: x.split('|')[2] if len(x.split('|')) == 3 else np.nan)
+        df[col] = df[col].apply(lambda x: x.split('|')[2])
 
     for col in ['DocumentDate', 'DueDate', 'ClearingDate']:
-        df[col] = pd.to_datetime(df[col], errors='coerce')
-
-    for col in ['DocumentType', 'PaymentTerms']:
-        df[col] = df[col].apply(lambda x: int(''.join(format(ord(w), '') for w in str(x))))
+        df[col] = pd.to_datetime(df[col])
 
     for col in ['DocumentKey', 'CustomerKey', 'InvoicedAmount']:
-        df[col] = pd.to_numeric(df[col], errors='coerce')
+        df[col] = pd.to_numeric(df[col])
+
+    df = df[(df['DocumentDate'] < df['DueDate']) & (df['DocumentDate'] < df['ClearingDate'])]
 
     return df
 
@@ -49,8 +37,7 @@ df1 = load_dataset('./data/base0a.csv')
 df2 = load_dataset('./data/base0b.csv')
 df = pd.concat([df1, df2])
 
-df = df[(df['DocumentDate'] < df['DueDate']) & (df['DocumentDate'] < df['ClearingDate'])]
-df = df[df['InvoicedAmount'] >= 1000]
+print('Final size', len(df))
 
 df.sort_values(by=['DueDate'], ascending=True, ignore_index=True, inplace=True)
 df.to_csv('./data/base1.csv', index=False)
