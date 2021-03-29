37d47af5be14467dd7b569843a3879cf1f8f3849
Arthur Flôr <arthurflor23@gmail.com>
2021-03-29 08:29:24 -0300

    Update scrip1

--- dso/step1-organize.py
+++ dso/step1-organize.py
@@ -14,6 +14,7 @@ def merge(x1, x2):
 
 
 df = pd.read_csv('./data/base1.csv', parse_dates=['DocumentDate', 'DueDate', 'ClearingDate'], low_memory=False)
+df['PaidLate'] = ((df['ClearingDate'] - df['DueDate']).astype('timedelta64[D]') > 0).astype(int)
 
 df_doc = order(group(df)['DocumentDate'].min().reset_index())
 df_cle = order(group(df)['ClearingDate'].max().reset_index())
@@ -25,23 +26,21 @@ df_gp = pd.DataFrame({
     'ClearingDate': df_cle['ClearingDate'].values,
 })
 
-df['PaidLate'] = ((df['ClearingDate'] - df['DueDate']).astype('timedelta64[D]') > 0).astype(int)
+df_gp = merge(df_gp, group(df).size().reset_index(name='InvoiceCount'))
+df_gp = merge(df_gp, group(df[df['PaidLate'] == 1]).size().reset_index(name='OSInvoiceCount'))
 
-df_gp = merge(df_gp, group(df).size().reset_index(name='Count'))
-df_gp = merge(df_gp, group(df[df['PaidLate'] == 1]).size().reset_index(name='OSCount'))
+df_gp['OSInvoiceCount'] = df_gp['OSInvoiceCount'].fillna(0).astype(int)
 
-df_gp['OSCount'] = df_gp['OSCount'].fillna(0).astype(int)
+df_gp['OSInvoiceCountRatio'] = df_gp['OSInvoiceCount'] / df_gp['InvoiceCount']
+df_gp['OSInvoiceCountRatio'] = df_gp['OSInvoiceCountRatio'].fillna(0)
 
-df_gp['OSCountRatio'] = df_gp['OSCount'] / df_gp['Count']
-df_gp['OSCountRatio'] = df_gp['OSCountRatio'].fillna(0)
+df_gp = merge(df_gp, group(df)['InvoicedAmount'].sum().reset_index(name='InvoiceAmount'))
+df_gp = merge(df_gp, group(df[df['PaidLate'] == 1])['InvoicedAmount'].sum().reset_index(name='OSInvoiceAmount'))
 
-df_gp = merge(df_gp, group(df)['InvoicedAmount'].sum().reset_index(name='Amount'))
-df_gp = merge(df_gp, group(df[df['PaidLate'] == 1])['InvoicedAmount'].sum().reset_index(name='OSAmount'))
+df_gp['OSInvoiceAmount'] = df_gp['OSInvoiceAmount'].fillna(0)
 
-df_gp['OSAmount'] = df_gp['OSAmount'].fillna(0)
-
-df_gp['OSAmountRatio'] = df_gp['OSAmount'] / df_gp['Amount']
-df_gp['OSAmountRatio'] = df_gp['OSAmountRatio'].fillna(0)
+df_gp['OSInvoiceAmountRatio'] = df_gp['OSInvoiceAmount'] / df_gp['InvoiceAmount']
+df_gp['OSInvoiceAmountRatio'] = df_gp['OSInvoiceAmountRatio'].fillna(0)
 
 print('Final size', len(df_gp))
 
