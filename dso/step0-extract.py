3f5cded18be43a72f51999523d7b41ee6efe9898
Arthur Flôr <arthurflor23@gmail.com>
2021-03-29 08:29:15 -0300

    Update scrip0

--- dso/step0-extract.py
+++ dso/step0-extract.py
@@ -3,10 +3,10 @@ import pandas as pd
 
 def load_dataset(filename):
     df = pd.read_csv(filename, low_memory=False)
-    print('Total size:\t\t\t\t', len(df))
+    print('Total size:\t\t\t\t\t', len(df))
 
     df = df[df['TransactionType'] == 'FI-InvoicedDocument']
-    print('Total size (transaction type filter):\t', len(df))
+    print('Total size (transaction type filter):\t\t', len(df))
 
     df = df[[
         'DocumentKey',
@@ -17,7 +17,7 @@ def load_dataset(filename):
         'InvoicedAmount',
     ]].dropna()
 
-    print('Total size (dropna filter):\t\t', len(df), '\n')
+    print('Total size (dropna filter):\t\t\t', len(df))
 
     for col in ['DocumentKey']:
         df[col] = df[col].apply(lambda x: x.split('|')[2])
@@ -29,14 +29,17 @@ def load_dataset(filename):
         df[col] = pd.to_numeric(df[col])
 
     df = df[(df['DocumentDate'] < df['DueDate']) & (df['DocumentDate'] < df['ClearingDate'])]
-    df = df[df['InvoicedAmount'] > 1000]
+    df = df[df['InvoicedAmount'] >= 1000]
+
+    print('Total size (Date and Invoice Amount filters):\t', len(df), '\n')
 
     return df
 
 
 df1 = load_dataset('./data/base0a.csv')
 df2 = load_dataset('./data/base0b.csv')
-df = pd.concat([df1, df2])
+df3 = load_dataset('./data/base0c.csv')
+df = pd.concat([df1, df2, df3])
 
 print('Final size', len(df))
 
