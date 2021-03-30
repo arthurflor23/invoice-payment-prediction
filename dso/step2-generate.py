8b0c039061b943f4b085be6bc520eb5ea49f5bbe
Arthur Flôr <arthurflor23@gmail.com>
2021-03-30 11:56:23 -0300

    Update weekday subtract script 2 Add After Month features

--- dso/step2-generate.py
+++ dso/step2-generate.py
@@ -8,46 +8,70 @@ import pandas as pd
 
 def fx(x, window_size=120):
     due_date = pd.to_datetime(x[1])
-    past_due_date = due_date - pd.DateOffset(months=window_size)
+
+    end_due_date = pd.to_datetime(due_date - pd.to_timedelta(arg=due_date.weekday(), unit='D'))
+    start_due_date = end_due_date - pd.DateOffset(months=window_size)
 
     c_data = df[df['CustomerKey'] == x[0]]
 
-    historic = c_data[(c_data['DueDate'] >= past_due_date) &
-                      (c_data['DueDate'] < due_date)]
-    historic_late = historic[historic['PaidLate'] == 1]
+    h = c_data[(c_data['DueDate'] >= start_due_date) & (c_data['DueDate'] < end_due_date)]
+    h_late = h[h['PaymentCategory'] == 1]
+    h_very_late = h[h['PaymentCategory'] == 2]
 
-    outstanding = c_data[(c_data['DocumentDate'] >= past_due_date) &
-                         (c_data['DocumentDate'] < due_date) &
-                         (c_data['ClearingDate'] > due_date) &
-                         (c_data['DueDate'] != due_date)]
-    outstanding_late = outstanding[outstanding['PaidLate'] == 1]
+    o = c_data[(c_data['DocumentDate'] >= start_due_date) & (c_data['DocumentDate'] < end_due_date) &
+               (c_data['ClearingDate'] > end_due_date) & (c_data['DueDate'] != due_date)]
+    o_late = o[o['PaymentCategory'] == 1]
+    o_very_late = o[o['PaymentCategory'] == 2]
 
     with np.errstate(divide='ignore', invalid='ignore'):
         features = {
             'CustomerKey': x[0],
             'DueDate': due_date,
 
-            'MAD_DaysLate': historic['DaysLate'].mad(),
-            'MAD_OSDaysLate': outstanding['DaysLate'].mad(),
+            'MAD_DaysLate': h_late['DaysLate'].mad(),
+            'MED_DaysLate': h_late['DaysLate'].median(),
 
-            'MED_DaysLate': historic['DaysLate'].median(),
-            'MED_OSDaysLate': outstanding['DaysLate'].median(),
+            'MAD_DaysLateAM': h_very_late['DaysLate'].mad(),
+            'MED_DaysLateAM': h_very_late['DaysLate'].median(),
 
-            'PaidCount': historic['InvoiceCount'].sum(),
-            'PaidLateCount': historic_late['InvoiceCount'].sum(),
-            'R_PaidLateCount': np.true_divide(historic_late['InvoiceCount'].sum(), historic['InvoiceCount'].sum()),
 
-            'PaidAmount': historic['InvoiceAmount'].sum(),
-            'PaidLateAmount': historic_late['InvoiceAmount'].sum(),
-            'R_PaidLateAmount': np.true_divide(historic_late['InvoiceAmount'].sum(), historic['InvoiceAmount'].sum()),
+            'MAD_OSDaysLate': o_late['DaysLate'].mad(),
+            'MED_OSDaysLate': o_late['DaysLate'].median(),
 
-            'OSCount': outstanding['InvoiceCount'].sum(),
-            'OSLateCount': outstanding_late['InvoiceCount'].sum(),
-            'R_OSLateCount': np.true_divide(outstanding_late['InvoiceCount'].sum(), outstanding['InvoiceCount'].sum()),
+            'MAD_OSDaysLateAM': o_very_late['DaysLate'].mad(),
+            'MED_OSDaysLateAM': o_very_late['DaysLate'].median(),
 
-            'OSAmount': outstanding['InvoiceAmount'].sum(),
-            'OSLateAmount': outstanding_late['InvoiceAmount'].sum(),
-            'R_OSLateAmount': np.true_divide(outstanding_late['InvoiceAmount'].sum(), outstanding['InvoiceAmount'].sum()),
+
+            'PaidCount': h['InvoiceCount'].sum(),
+            'PaidLateCount': h_late['InvoiceCount'].sum(),
+            'PaidLateAMCount': h_very_late['InvoiceCount'].sum(),
+
+            'R_PaidLateCount': np.true_divide(h_late['InvoiceCount'].sum(), h['InvoiceCount'].sum()),
+            'R_PaidLateAMCount': np.true_divide(h_very_late['InvoiceCount'].sum(), h['InvoiceCount'].sum()),
+
+
+            'PaidAmount': h['InvoiceAmount'].sum(),
+            'PaidLateAmount': h_late['InvoiceAmount'].sum(),
+            'PaidLateAMAmount': h_very_late['InvoiceAmount'].sum(),
+
+            'R_PaidLateAmount': np.true_divide(h_late['InvoiceAmount'].sum(), h['InvoiceAmount'].sum()),
+            'R_PaidLateAMAmount': np.true_divide(h_very_late['InvoiceAmount'].sum(), h['InvoiceAmount'].sum()),
+
+
+            'OSCount': o['InvoiceCount'].sum(),
+            'OSLateCount': o_late['InvoiceCount'].sum(),
+            'OSLateAMCount': o_very_late['InvoiceCount'].sum(),
+
+            'R_OSLateCount': np.true_divide(o_late['InvoiceCount'].sum(), o['InvoiceCount'].sum()),
+            'R_OSLateAMCount': np.true_divide(o_very_late['InvoiceCount'].sum(), o['InvoiceCount'].sum()),
+
+
+            'OSAmount': o['InvoiceAmount'].sum(),
+            'OSLateAmount': o_late['InvoiceAmount'].sum(),
+            'OSLateAMAmount': o_very_late['InvoiceAmount'].sum(),
+
+            'R_OSLateAmount': np.true_divide(o_late['InvoiceAmount'].sum(), o['InvoiceAmount'].sum()),
+            'R_OSLateAMAmount': np.true_divide(o_very_late['InvoiceAmount'].sum(), o['InvoiceAmount'].sum()),
         }
 
     for k in features.keys():
@@ -58,8 +82,13 @@ def fx(x, window_size=120):
 
 df = pd.read_csv('./data/base2.csv', parse_dates=['DocumentDate', 'DueDate', 'ClearingDate'], low_memory=False)
 
-df['DaysLate'] = ((df['ClearingDate'] - df['DueDate']).astype('timedelta64[D]')).astype(int)
-df['PaidLate'] = (df['DaysLate'] > 0).astype(int)
+df['DaysToEndMonth'] = ((df['DueDate'] + pd.offsets.MonthEnd(0)) - df['DueDate']).dt.days
+df['DaysLate'] = ((df['ClearingDate'] - df['DueDate']).dt.days).clip(lower=0)
+df['DaysLateAM'] = (df['DaysLate'] - df['DaysToEndMonth']).clip(lower=0)
+
+df['PaymentCategory'] = 0
+df.loc[(df['DaysLate'] > 0) & (df['DaysLateAM'] <= 0), 'PaymentCategory'] = 1
+df.loc[(df['DaysLate'] > 0) & (df['DaysLateAM'] > 0), 'PaymentCategory'] = 2
 
 features = []
 with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
@@ -69,7 +98,7 @@ with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
     pool.join()
 
 df = pd.merge(df, pd.DataFrame(features), how='left', on=['CustomerKey', 'DueDate'])
-df.drop(['PaidLate', 'DaysLate'], axis=1, inplace=True)
+df.drop(['DaysToEndMonth', 'DaysLate', 'DaysLateAM', 'PaymentCategory'], axis=1, inplace=True)
 
 df.sort_values(by=['DueDate'], ascending=True, ignore_index=True, inplace=True)
 df.to_csv('./data/base3.csv', index=False)
