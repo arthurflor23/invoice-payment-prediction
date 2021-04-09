25b03f0ce671fdba30afb76ae7e47f903e1ee11f
Arthur Flôr <arthurflor23@gmail.com>
2021-04-09 00:01:26 -0300

    Fix issues

--- dso/src/model.py
+++ dso/src/model.py
@@ -32,9 +32,10 @@ class Dataset():
 
     def load(self, step, train=True, test=True):
         df = pd.read_csv(self.filename, parse_dates=['DueDate'], low_memory=False)
+        df['DaysLateAM'].clip(upper=30, inplace=True)
 
-        t1 = df[df['DueDate'] < self.date].drop(['DueDate'], axis=1)
-        t2 = df[(df['DueDate'] >= self.date) & (df['DueDate'] < self.date + self.offset)].drop(['DueDate'], axis=1)
+        t1 = df[df['DueDate'] < self.date]
+        t2 = df[(df['DueDate'] >= self.date) & (df['DueDate'] < self.date + self.offset)]
 
         if step == 1:
             self.y = 'PaidLate'
@@ -79,8 +80,6 @@ class Model():
 
         self.gridsearch = json.load(open(self.searchfile)) if os.path.exists(self.searchfile) else None
         self.hyper = json.load(open(self.hyperfile)) if os.path.exists(self.hyperfile) else {}
-
-        self.cmap = 'Blues' if step == 1 else 'YlOrBr'
         self.output = os.path.join('..', f'output{step}', self.estimator)
 
     def tunning(self, x_train, y_train):
@@ -97,7 +96,7 @@ class Model():
                 md = md.set_params(**base_param)
 
         elif is_regressor(self.model):
-            scoring = 'neg_root_mean_squared_error'
+            scoring = None
             md = self.model(random_state=SEED)
             cv = ShuffleSplit(n_splits=1, train_size=0.8, random_state=SEED)
 
@@ -120,7 +119,9 @@ class Model():
         md = pickle.load(open(os.path.join(self.output, 'model.sav'), 'rb'))
 
         pd_test = md.predict(x_test)
-        self._report(y_test, pd_test, md, prefix='test', cmap=self.cmap)
+        matches = self._report(y_test, pd_test, md, prefix='test')
+
+        return y_test, pd_test, matches, self.output
 
     def train(self, x_train, y_train):
         md = self.model(random_state=SEED)
@@ -128,16 +129,19 @@ class Model():
         if 'base_estimator' in md.get_params() and md.get_params()['base_estimator'] is None:
             if is_classifier(self.model):
                 base_param = {'base_estimator': DecisionTreeClassifier(random_state=SEED)}
+
             elif is_regressor(self.model):
                 base_param = {'base_estimator': DecisionTreeRegressor(random_state=SEED)}
 
-        if self.estimator in self.hyper.keys():
             md = md.set_params(**base_param)
 
-        md.set_params(**self.hyper[self.estimator]['params'])
+        if self.estimator in self.hyper.keys():
+            md.set_params(**self.hyper[self.estimator]['params'])
 
         gt_train, pd_train, md = self._cross_validation(md, x_train, y_train, run_only_once=True)
-        self._report(gt_train, pd_train, md, prefix='train', cmap=self.cmap)
+        matches = self._report(gt_train, pd_train, md, prefix='train')
+
+        return gt_train, pd_train, matches, self.output
 
     def _cross_validation(self, model, x, y, n_splits=10, n_repeats=3, run_only_once=False):
         if is_classifier(model):
@@ -167,40 +171,90 @@ class Model():
 
         return gt, pred, model_f
 
-    def _report(self, gt, pred, model, prefix, cmap='Blues'):
+    def _report(self, gt, pred, model, prefix):
         os.makedirs(self.output, exist_ok=True)
         pickle.dump(model, open(os.path.join(self.output, 'model.sav'), 'wb'))
+        matches = None
 
         if is_classifier(self.model):
             with open(os.path.join(self.output, f'{prefix}_classification_report.txt'), 'w') as f:
                 report = classification_report(gt, pred, digits=4, zero_division=True)
                 f.write(''.join(report))
-                print(report)
-
-            fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8, 8))
-            g = sns.heatmap(confusion_matrix(gt, pred), fmt='d', square=True, annot=True, cmap=cmap, ax=ax)
-            g.get_figure().savefig(os.path.join(self.output, f'{prefix}_heatmap.png'))
+                print(''.join(report))
 
         elif is_regressor(self.model):
             with open(os.path.join(self.output, f'{prefix}_rmse_accuracy.txt'), 'w') as f:
-                report = self._accuracy_tolerance(gt, pred, tolerance=[0, 1, 2, 3])
+                matches, report = self._acc_tolerance(gt, pred, tolerance=[0, 1, 2, 3])
                 f.write('\n'.join(report))
                 print('\n'.join(report))
 
-    def _accuracy_tolerance(self, gt, pd, tolerance=[0]):
-        rmse = np.sqrt(mean_squared_error(gt, pd))
-        report = [f'RMSE: {rmse:.8}']
+        return matches
+
+    def _acc_tolerance(self, gt, pred, tolerance=[0]):
+        matches, report = [], [f'RMSE: {mean_squared_error(gt, pred, squared=False):.8}']
 
         for t in tolerance:
-            match = 0
+            match = []
 
             for i, x in enumerate(gt):
-                match += 1 if pd[i] >= (x - t) and pd[i] <= (x + t) else 0
+                p = np.array(pred[i], dtype=int)
+                match.append(1 if p >= (x - t) and p <= (x + t) else 0)
 
-            acc = ((match / len(gt)) * 100)
-            report.append(f'Accuracy with ±{t} days of tolerance (range of {t*2} days): {acc:.8}%')
+            matches.append(np.array(match))
+            acc = '{:.2f}%'.format(np.mean(match) * 100)
+            report.append(f'Accuracy with ±{t} days of tolerance (range of {t*2} days): {acc}')
 
-        return report
+        return [tolerance, matches], report
+
+
+def plot_classifier_report(y, pred, output, prefix, cmap='Blues'):
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8, 8))
+    g = sns.heatmap(confusion_matrix(y, pred), fmt='d', square=True, annot=True, cmap=cmap, ax=ax)
+    g.get_figure().savefig(os.path.join(output, f'{prefix}_heatmap.png'))
+
+
+def plot_regressor_report(matches, output, prefix):
+    x_label, y_label = 'Tolerância', 'Acurácia'
+    x_labels = [f'±{x} dias' for x in matches[0]]
+    p_labels = list(range(0, 101, 10))
+
+    df1 = pd.DataFrame({x_label: x_labels, y_label: np.mean(matches[1], axis=1) * 100})
+    ax = df1.plot.bar(x=x_label, y=y_label, yticks=p_labels, figsize=(8, 5), fontsize=14, rot=0, legend=False)
+
+    plt.xlabel(x_label, fontsize=14)
+    plt.ylabel(f'{y_label} (%)', fontsize=14)
+
+    for i, rect in enumerate(ax.patches):
+        x = rect.get_x() + rect.get_width() / 2
+        xy = rect.get_height() + 1
+        ax.text(x, xy, '{:.2f}%'.format(df1[y_label][i]), ha='center', va='bottom', fontsize=12)
+
+    plt.savefig(os.path.join(output, f'{prefix}_accuracy.png'))
+
+
+def plot_regressor_per_day_report(matches, output, prefix, days):
+    x_labels = 'Dia do Mês'
+    y_labels = [f'±{x} dias' for x in matches[0]]
+    p_labels = list(range(0, 101, 10))
+    m_labels = list(range(0, 31, 2))
+
+    columns = {'Days': days}
+
+    for i, x in enumerate(y_labels):
+        columns[x] = matches[1][i]
+
+    df1 = pd.DataFrame(columns)
+    columns = {x_labels: df1.groupby(['Days']).groups.keys()}
+
+    for i, x in enumerate(y_labels):
+        columns[x] = df1.groupby(['Days'])[x].mean() * 100
+
+    df2 = pd.DataFrame(columns)
+    df2.plot.line(x=x_labels, y=y_labels, yticks=p_labels, xticks=m_labels, figsize=(8, 5), fontsize=14, rot=0)
+
+    plt.ylabel('Acurácia (%)', fontsize=14)
+    plt.xlabel(x_labels, fontsize=14)
+    plt.savefig(os.path.join(output, f'{prefix}_accuracy_per_day.png'))
 
 
 if __name__ == "__main__":
@@ -271,18 +325,25 @@ if __name__ == "__main__":
 
     if arg.action == 'tunning':
         dataset.load(step=arg.step, train=True, test=False)
-
         model = Model(arg.estimator, step=arg.step)
         model.tunning(dataset.x_train, dataset.y_train)
 
-    elif arg.action == 'train':
-        dataset.load(step=arg.step, train=True, test=False)
+    else:
+        if arg.action == 'train':
+            dataset.load(step=arg.step, train=True, test=False)
+            model = Model(arg.estimator, step=arg.step)
+            y, p, m, o = model.train(dataset.x_train, dataset.y_train)
 
-        model = Model(arg.estimator, step=arg.step)
-        model.train(dataset.x_train, dataset.y_train)
+        elif arg.action == 'test':
+            dataset.load(step=arg.step, train=False, test=True)
+            model = Model(arg.estimator, step=arg.step)
+            y, p, m, o = model.test(dataset.x_test, dataset.y_test)
 
-    elif arg.action == 'test':
-        dataset.load(step=arg.step, train=False, test=True)
+        if arg.step == 1 or arg.step == 2:
+            plot_classifier_report(y, p, o, prefix=arg.action, cmap=('Blues' if arg.step == 1 else 'YlOrBr'))
 
-        model = Model(arg.estimator, step=arg.step)
-        model.test(dataset.x_test, dataset.y_test)
+        elif arg.step == 3:
+            plot_regressor_report(m, o, prefix=arg.action)
+
+            if arg.action == 'test':
+                plot_regressor_per_day_report(m, o, prefix='test', days=dataset.test['DueDate'].dt.day)
